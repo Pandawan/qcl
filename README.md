4d8e2173cc5ee430b22086eb20b55bd875f02e6a
KhoomeiK <rohan.pandey@gmail.com>
2019-02-04 08:23:05 -0800

    oowoo

--- README.md
+++ README.md
@@ -9,3 +9,7 @@ Node.js server indexes npm packages and compiles using pkg. When receiving reque
 ## Client
 
 When user runs "qcl [package]", sends GET request to server for binary and pushes to /usr/bin/local or correct directory. config.json defines configuration but deletes binaries after 48 hours by default.
+
+## Monetization Plan
+
+Client is open source and can be downloaded/run for free. When used free, it uses source code from npm rather than binaries, resulting in slightly slower download times but still has shortterm/cleanup functionality. When the user begins paying, they get a key included in the config.json that allows them to get binaries and some other functionality from backend.
--- server/index.js
+++ server/index.js
@@ -1,13 +1,33 @@
 const express = require('express');
+const cmd = require('node-cmd');
 const app = express();
 const port = 8000;
 
-app.get('/pkg/*', (req, res) => {
-	let p = req.path.slice(1);
-	console.log(p);
+app.get('/pkg/*', async (req, res) => {
+	let p = req.path.slice(5);
+	let o = clientData(req.headers["user-agent"]);
 
-	download(p);
-	compile(p);
+	let b;
+	check(p, done => {
+		b = done;
+		console.log(b);
+	});
+
+	// if (b === true) {
+	// 	console.log("true")
+	// }
+	// else if (b === false) {
+	// 	console.log("false")
+	// }
+	// else if (b === undefined) {
+	// 	console.log("undefined")
+	// }
+	// else {
+	// 	console.log("lol")
+	// }
+
+	// await download(p);
+	// await compile(p);
 
 	/* TODO:
 	>Check for userKey and userOS in req.params (maybe IP too)
@@ -20,15 +40,38 @@ app.get('/pkg/*', (req, res) => {
 		>If not, npm install package, run pkg, then sendFile
 	*/
 
-	res.sendFile(`/Users/rohan/Documents/Webwork/quicli/server/bin/${p}-userOS`); // userOS
+	// res.sendFile(`/Users/rohan/Documents/Webwork/quicli/server/bin/${p}-${o}`);
+	res.send(p);
 });
 
-download = () => {
-	// npm programmaticaly (fs?)
+clientData = a => {
+	if (a.includes("Macintosh"))
+		return "macos";
+	else if (a.includes("Windows"))
+		return "win.exe";
+	else if (a.includes("Linux"))
+		return "linux";
+	else
+		return 404;
+	// ip?
 }
 
-compile = () => {
-	// pkg
+check = async (p, cb) => {
+	await cmd.get('ls ./mods/node_modules', (err, data, stderr) => {
+		cb(data.includes(p));
+	});
+}
+
+download = async p => {
+	cmd.get(`npm install --prefix ./mods/ ${p}`, (err, data, stderr) => {
+		console.log(data);
+	});
+}
+
+compile = async p => {
+	cmd.get(`pkg --out-path ./bin ./mods/node_modules/${p}`, (err, data, stderr) => {
+		console.log(data);
+	});
 }
 
 app.get('/createKey', (req, res) => {
